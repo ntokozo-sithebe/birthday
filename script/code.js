044b9abdee1e596da3a021bea1cc230d6784a694
ntokozo-sithebe <ntoky28@gmail.com>
2023-11-20 15:11:37 +0200

    birthday

---
+++ script/code.js
@@ -0,0 +1,35 @@
+//let input1 = Date
+//let input2 = 'text'
+//input1 + input2 = document.getElementsByName('button')
+
+
+let date =  document.getElementsByTagName(input) 
+   // input1 + input2 = document.getElementsByName('button')
+    
+
+/* step by step 
+Result - we want to diplay the outcome of birthdays
+
+*/
+Display = input1 + input2
+btnDisplay = document.querySelectorAll('[type]')
+
+// 
+
+const numbOfDays = difference / (1000 * 3600 * 24)
+// Difference between dates 
+
+/*let birthday = {
+    year: '2001',
+    date: "18 July",
+    birthday: function(){
+        return this.input 
+
+}
+}*/
+ // button will display the captured data of both the input values
+
+ let btnDisplay = document.querySelector('[data.text]')       // eval captures data ...?
+ btnDisplay.addEventListener('click', ()=>{
+    eval.input
+ })
